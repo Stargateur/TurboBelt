e90c4a9c8d814a6c457e9e0da667dbd6327dd253
Antoine PLASKOWSKI <plaskowski.stanislas@gmail.com>
2025-08-08 12:35:50 +0200

    0.0.6

--- data.lua
+++ data.lua
@@ -186,27 +186,39 @@ if mods["aai-loaders"] then
   AAILoaders.make_tier(turbo_specs)
 end
 
-if mods["comfortable-loader"] then
-    if data.raw["loader-1x1"]["express-comfortable-loader"] then
-        data.raw["loader-1x1"]["express-comfortable-loader"].next_upgrade = "turbo-comfortable-loader"
-    end
+if mods["deadlock-beltboxes-loaders"] then
+	-- add the upgrade for t3 only if space age is present and we're gonna be loading t4
+	if data.raw["loader-1x1"]["express-transport-belt-loader"] then
+		data.raw["loader-1x1"]["express-transport-belt-loader"].next_upgrade = "turbo-transport-belt-loader"
+	end
+	if data.raw.furnace["express-transport-belt-beltbox"] then
+		data.raw.furnace["express-transport-belt-beltbox"].next_upgrade = "turbo-transport-belt-beltbox"
+	end
 
-    --tier 4
-    deadlock.add_tier({
-        transport_belt = "turbo-transport-belt",
-        loader = "turbo-comfortable-loader",
-
-        colour = { r = 129, g = 139, b = 90 },
-        underground_belt = "turbo-underground-belt",
-        splitter = "turbo-splitter",
-        technology = "turbo-transport-belt",
-        order = "d",
-        loader_ingredients = {
-            { type = "item", name = "express-comfortable-loader", amount = 1 },
-            { type = "item", name = "iron-gear-wheel", amount = 40 },
-            { type = "item", name = "uranium-238", amount = 10 },
-            { type = "fluid", name = "lubricant", amount = 20 },
-        },
-        loader_category = "crafting-with-fluid",
-    })
+	-- tier 4
+	deadlock.add_tier({
+		transport_belt      = "turbo-transport-belt",
+		colour              = {r=160, g=190, b=80},
+		underground_belt    = "turbo-underground-belt",
+		splitter            = "turbo-splitter",
+		technology          = "turbo-transport-belt",
+		order               = "d",
+		loader_ingredients  = {
+			{name = "express-transport-belt-loader", type = "item", amount = 1},
+			{name = "uranium-238", type = "item", amount = 20},
+			{name = "lubricant", type = "fluid", amount = 20},
+		},
+		loader_category     = "crafting-with-fluid",
+		beltbox_ingredients = {
+			{name = "express-transport-belt-beltbox", type = "item", amount = 1},
+			{name = "uranium-238", type = "item", amount = 15},
+			{name = "iron-gear-wheel", type = "item", amount = 15},
+			{name = "lubricant", type = "fluid", amount = 100},
+		},
+		beltbox_category    = "crafting-with-fluid",
+		beltbox_technology  = "deadlock-stacking-4",
+	})
+	if data.raw.technology["deadlock-stacking-4"] then
+		table.insert(data.raw.technology["deadlock-stacking-4"].prerequisites, "deadlock-stacking-3")
+	end
 end
